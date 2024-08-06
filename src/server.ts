b8dce0a1fda6154015caa747414a3c8f09182dc1
lucasbrun196 <luucaasbrum13@gmail.com>
2024-08-06 01:53:09 -0300

    create server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import * as http from "http";
+
+const PORT = process.env.PORT
+const server : http.Server = http.createServer((requeste : http.IncomingMessage , response : http.ServerResponse) => {
+
+});
+
+server.listen(PORT, () => {
+    console.log(`Running server on port: ${PORT}`); 
+});
+
