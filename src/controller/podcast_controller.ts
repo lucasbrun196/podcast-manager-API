788bf4dfb6df589cfff1978e24bc8540e77cb614
lucasbrun196 <luucaasbrum13@gmail.com>
2024-08-07 01:56:40 -0300

    controller podcast

---
+++ src/controller/podcast_controller.ts
@@ -0,0 +1,8 @@
+import { IncomingMessage, ServerResponse } from 'http'; 
+import { serviceListEp } from '../service/list_ep_service';
+
+export const getListEp = async (req : IncomingMessage, res : ServerResponse) => {
+    const content : Object = await serviceListEp();
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify(content));
+}
