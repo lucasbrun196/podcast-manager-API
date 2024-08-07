5dabd3a00c687f8edd1d912724bde6a225bef4ff
lucasbrun196 <luucaasbrum13@gmail.com>
2024-08-07 01:56:15 -0300

    server req res

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import * as http from "http";
+import { getListEp } from "./controller/podcast_controller";
 
 const PORT = process.env.PORT
-const server : http.Server = http.createServer((requeste : http.IncomingMessage , response : http.ServerResponse) => {
-
+const server : http.Server = http.createServer( async (req : http.IncomingMessage , res : http.ServerResponse) => {
+    if(req.method === "GET"){
+        await getListEp(req, res);
+    }
 });
 
 server.listen(PORT, () => {
