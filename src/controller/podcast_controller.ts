7f4472ad04429c581e1e30c84b4d6022fd3c0d37
lucasbrun.196 <luucaasbrum13@gmail.com>
2024-09-25 20:02:43 -0300

    write in body response

--- src/controller/podcast_controller.ts
+++ src/controller/podcast_controller.ts
@@ -1,19 +1,20 @@
 import { IncomingMessage, ServerResponse } from 'http'; 
 import { serviceListEp } from '../service/list_ep_service';
 import { serviceFilterEp } from '../service/filter_ep_service';
-import { StatusCode } from '../utils/status_code';
 import { ContentType } from '../utils/content_type';
 import { FilterPodCastModel } from '../models/filter_podcast_model';
 
 export const getListEp = async (req : IncomingMessage, res : ServerResponse) => {
     const content : FilterPodCastModel = await serviceListEp();
     res.writeHead(content.statusCode, {"Content-Type": ContentType.JSON});
-    res.end(JSON.stringify(content.body));
+    res.write(JSON.stringify(content.body));
+    res.end()
 }
 
 
 export const getFilterEp = async (req: IncomingMessage, res: ServerResponse) => {
     const content: FilterPodCastModel = await serviceFilterEp(req.url);
     res.writeHead(content.statusCode, {"Content-Type": ContentType.JSON});
-    res.end(JSON.stringify(content.body));
+    res.write(JSON.stringify(content.body));
+    res.end()
 } 
